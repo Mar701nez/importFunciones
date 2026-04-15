acdcc591b97930903f32967abbb99b2b2369572d
mar71nez <mar701nez@gmail.com>
2026-04-15 09:31:23 -0500

    chore(API): agrego funcion factorial

--- src/app.js
+++ src/app.js
@@ -23,3 +23,15 @@ export function healtCheack () {
     version: '1.1.0'
   };
 }
+
+export function factorial(n) {
+    if (n < 0) return "Error: No existe factorial de números negativos";
+    if (n === 0 || n === 1) return 1;
+    
+    let resultado = 1;
+    for (let i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
